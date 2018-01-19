3944d0020d717d16e439c536bc1a72fd4040da65
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-01-19 12:30:25 -0700

    Added another number to the RubiksButtons ID's to represent orientation.

--- src/rubiks/view/BottomPanel.java
+++ src/rubiks/view/BottomPanel.java
@@ -12,14 +12,14 @@ public class BottomPanel extends FacePanel
 	
 	private void setupPanel()
 	{
-		this.buttonArray[0].setID(new int[] {0, 2, 0});
-		this.buttonArray[1].setID(new int[] {0, 2, 1});
-		this.buttonArray[2].setID(new int[] {0, 2, 2});
-		this.buttonArray[3].setID(new int[] {1, 2, 0});
-		this.buttonArray[4].setID(new int[] {1, 2, 1});
-		this.buttonArray[5].setID(new int[] {1, 2, 2});
-		this.buttonArray[6].setID(new int[] {2, 2, 0});
-		this.buttonArray[7].setID(new int[] {2, 2, 1});
-		this.buttonArray[8].setID(new int[] {2, 2, 2});
+		this.buttonArray[0].setID(new int[] {0, 2, 0, 3});
+		this.buttonArray[1].setID(new int[] {0, 2, 1, 3});
+		this.buttonArray[2].setID(new int[] {0, 2, 2, 3});
+		this.buttonArray[3].setID(new int[] {1, 2, 0, 3});
+		this.buttonArray[4].setID(new int[] {1, 2, 1, 3});
+		this.buttonArray[5].setID(new int[] {1, 2, 2, 3});
+		this.buttonArray[6].setID(new int[] {2, 2, 0, 3});
+		this.buttonArray[7].setID(new int[] {2, 2, 1, 3});
+		this.buttonArray[8].setID(new int[] {2, 2, 2, 3});
 	}
 }
--- src/rubiks/view/LeftPanel.java
+++ src/rubiks/view/LeftPanel.java
@@ -12,14 +12,14 @@ public class LeftPanel extends FacePanel
 	
 	private void setupPanel()
 	{
-		this.buttonArray[0].setID(new int[] {2, 0, 0});
-		this.buttonArray[1].setID(new int[] {1, 0, 0});
-		this.buttonArray[2].setID(new int[] {0, 0, 0});
-		this.buttonArray[3].setID(new int[] {2, 1, 0});
-		this.buttonArray[4].setID(new int[] {1, 1, 0});
-		this.buttonArray[5].setID(new int[] {0, 1, 0});
-		this.buttonArray[6].setID(new int[] {2, 2, 0});
-		this.buttonArray[7].setID(new int[] {1, 2, 0});
-		this.buttonArray[8].setID(new int[] {0, 2, 0});
+		this.buttonArray[0].setID(new int[] {2, 0, 0, 4});
+		this.buttonArray[1].setID(new int[] {1, 0, 0, 4});
+		this.buttonArray[2].setID(new int[] {0, 0, 0, 4});
+		this.buttonArray[3].setID(new int[] {2, 1, 0, 4});
+		this.buttonArray[4].setID(new int[] {1, 1, 0, 4});
+		this.buttonArray[5].setID(new int[] {0, 1, 0, 4});
+		this.buttonArray[6].setID(new int[] {2, 2, 0, 4});
+		this.buttonArray[7].setID(new int[] {1, 2, 0, 4});
+		this.buttonArray[8].setID(new int[] {0, 2, 0, 4});
 	}
 }
