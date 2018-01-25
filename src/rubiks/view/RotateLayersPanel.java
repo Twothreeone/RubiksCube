6b9c2d85aa9cba558aa3c913137828b815912951
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-01-25 12:43:16 -0700

    Imported images of arrows and set the buttons on the RotateLayersPanel to them to give the program more life.

--- src/rubiks/view/RotateLayersPanel.java
+++ src/rubiks/view/RotateLayersPanel.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -51,7 +52,26 @@ public class RotateLayersPanel extends JPanel
 		panelTextArea.setEditable(false);
 		panelTextArea.setForeground(Color.WHITE);
 		panelTextArea.setBackground(Color.DARK_GRAY);
+		up.setIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/upArrow.png")));
+		up.setPressedIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/upArrowPressed.png")));
+		up.setRolloverIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/upArrowRollover.png")));
+		up.setBackground(Color.DARK_GRAY);
+		up.setBorderPainted(false);
+		right.setIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/rightArrow.png")));
+		right.setPressedIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/rightArrowPressed.png")));
+		right.setRolloverIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/rightArrowRollover.png")));
+		right.setBackground(Color.DARK_GRAY);
+		right.setBorderPainted(false);
+		down.setIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/downArrow.png")));
+		down.setPressedIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/downArrowPressed.png")));
+		down.setRolloverIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/downArrowRollover.png")));
+		down.setBackground(Color.DARK_GRAY);
+		down.setBorderPainted(false);
+		left.setIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/leftArrow.png")));
+		left.setPressedIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/leftArrowPressed.png")));
+		left.setRolloverIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/leftArrowRollover.png")));
+		left.setBackground(Color.DARK_GRAY);
+		left.setBorderPainted(false);
 		this.setBackground(Color.DARK_GRAY);
-		
 	}
 }
