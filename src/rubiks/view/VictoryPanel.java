047af63d2e552389ac2a2b8b8afa962d7a693b8c
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-11 12:15:57 -0700

    Set up the location and look of the buttons.

--- src/rubiks/view/VictoryPanel.java
+++ src/rubiks/view/VictoryPanel.java
@@ -46,10 +46,12 @@ public class VictoryPanel extends JPanel
 		solved.setForeground(Color.WHITE);
 		greenFireworks.setIcon(new ImageIcon(getClass().getResource("/rubiks/view/images/greenFireworks.gif")));
 		greenFireworks.setHorizontalAlignment(SwingConstants.CENTER);
-		menu.setOpaque(true);
 		menu.setBorder(new LineBorder(Color.BLACK, 5));
-		tryAgain.setOpaque(true);
+		menu.setForeground(Color.WHITE);
+		menu.setFont(new Font("Lucida Grande", Font.BOLD, 35));
 		tryAgain.setBorder(new LineBorder(Color.BLACK, 5));
+		tryAgain.setForeground(Color.WHITE);
+		tryAgain.setFont(new Font("Lucida Grande", Font.BOLD, 35));
 		this.setBorder(new LineBorder(Color.BLACK, 5));
 	}
 	
@@ -62,6 +64,14 @@ public class VictoryPanel extends JPanel
 		springLayout.putConstraint(SpringLayout.WEST, greenFireworks, 0, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, greenFireworks, 0, SpringLayout.SOUTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, greenFireworks, 0, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, menu, 400, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, menu, 20, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, menu, -20, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, menu, 220, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, tryAgain, 400, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, tryAgain, -220, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, tryAgain, -20, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, tryAgain, -20, SpringLayout.EAST, this);
 	}
 	
 	private void setupListeners()
